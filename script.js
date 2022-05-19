6ad8bbec4cefeb35b0baeed9d800a1573b2f458f
MirThomasson <93678569+Mirthomasson@users.noreply.github.com>
2022-05-19 09:47:23 -0400

    star a fix

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
+const starWrapper = document.querySelector(".stars");
+const stars = document.querySelectorAll(".stars a");
 
-var header = document.getElementById("stars");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+  stars.forEach((star, clickedIdx) => {
+    star.addEventListener('click', () => {
+      starWrapper.classList.add("disabled")
+      stars.forEach((otherStar, otherIdx) => {
+        if (otherIdx <= clickedIdx) {
+          otherStar.classList.add("active");
+        }
+      })
+      console.log(`${clickedIdx + 1}`);
+      //Post
+
+    });
   });
-}
+
 
 
 document.getElementById("submit-button").onclick = function () {
   location.href = "./index-2.html";
+  
 };
 
